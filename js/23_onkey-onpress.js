8a45f5cd721d44f23fc4b12124e3fa61f698668e
sangjunchung <wjtkwn19@gmail.com>
2024-05-22 15:02:11 +0900

    onkey-onpress

--- js/23_onkey-onpress.js
+++ js/23_onkey-onpress.js
@@ -3,6 +3,8 @@ function 기능만들기() {
   기능.value = 기능.value.toLowerCase(); // 영어 소문자
   기능.value = 기능.value.toUpperCase(); // 영어 대문자
 }
-function 엔터기능() {
-  alert("입력한 내용이 전달되었습니다.");
+function 엔터기능(e) {
+  if (e.key === "Enter") {
+    alert("입력한 내용이 전달되었습니다.");
+  }
 }
